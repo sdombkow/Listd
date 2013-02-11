d5dbc644b17e1c1063ef287faf505c568767fcf9
Michael Wang <mwang4@andrew.cmu.edu>
2013-02-11 06:10:49 -0500

    Changed content and moved FB link and other small changes

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -33,9 +33,12 @@ class PurchasesController < ApplicationController
         @end_year = @customer_card.active_card.exp_year
         logger.error "#{@end_month < Time.now.month}"
         logger.error "#{@end_year < Time.now.year}"
-		    if @end_month < Time.now.month or @end_year < Time.now.year
+		logger.error "#{@end_month < Time.now.month || @end_year < Time.now.year}"
+		    if @end_month < Time.now.month || @end_year < Time.now.year
 		        redirect_to [@bar,@pass_set], notice: 'Sorry, your transaction has not occured. Your current saved card has expired.'
-		    end
+				return
+			end
+		logger.error "Something is wrong here"
 		    if @purchase.stripe_card_token == ""
 		        if @purchase.payment_return_customer(current_user)
 		            @pass_set.sold_passes+=num_passes
