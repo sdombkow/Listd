09013087e899e0be799096470349dc47e3423567
Scott Dombkowski <sdombkow@andrew.cmu.edu>
2013-02-16 12:12:54 -0800

    Regex Fix - 2/16

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,7 +6,7 @@ class Purchase < ActiveRecord::Base
   attr_accessible :stripe_card_token, :name, :date, :num_passes, :pass_set, :bar, :price
   
   validates :name, :bar, :pass_set, :price, :num_passes, :date, :presence => true
-  validates :name, :format => {:with => /\w+\s(\w+\s)+/, :message => "Name is not valid"}
+  validates :name, :format => {:with => /(\w+\s)(\w+-?.?\w?\s?)+/, :message => "Name is not valid"}
   
   
   def payment
