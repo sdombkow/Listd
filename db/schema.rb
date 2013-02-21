1019fa131d18e15793022e6903f5747a3cc7a93e
Scott Dombkowski <sdombkow@andrew.cmu.edu>
2013-02-20 23:50:21 -0800

    Installed Reservations with Selling_Passes boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211232539) do
+ActiveRecord::Schema.define(:version => 20130221065515) do
 
   create_table "bars", :force => true do |t|
     t.string   "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20130211232539) do
     t.decimal  "price",                 :precision => 10, :scale => 2
     t.datetime "created_at",                                           :null => false
     t.datetime "updated_at",                                           :null => false
+    t.boolean  "selling_passes"
   end
 
   add_index "pass_sets", ["bar_id"], :name => "index_pass_sets_on_bar_id"
