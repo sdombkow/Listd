26aa457256a751fafb2e99f79d4004e7652e366a
Scott Dombkowski <sdombkow@andrew.cmu.edu>
2013-02-22 21:52:16 -0800

    Changed Production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ ListdApp::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.precompile += ['application.css.scss']
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
